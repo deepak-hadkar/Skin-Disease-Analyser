a7cb7a5eae557b70d633bdf73dccc9854cba6e40
deepak.hadkar <deepakhadkar4@gmail.com>
2023-05-05 00:41:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ from streamlit_lottie import st_lottie_spinner
 import streamlit.components.v1 as components
 
 
-MODELSPATH = "/Users/deepakhadkar/Documents/GitHub/Projects/Paid/Skin-cancer-Analyzer-master/models/"
-DATAPATH = "/Users/deepakhadkar/Documents/GitHub/Projects/Paid/Skin-cancer-Analyzer-master/data/"
+MODELSPATH = "/Users/deepakhadkar/Documents/GitHub/Skin-Disease-Analyzer-master/models/"
+DATAPATH = "/Users/deepakhadkar/Documents/GitHub/Skin-Disease-Analyzer-master/data/"
 
 def render_header():
     st.write("""
@@ -123,8 +123,8 @@ def main():
         </div> 
         """
         # display the front end aspect
-        img1 = Image.open("/Users/deepakhadkar/Documents/GitHub/Projects/Paid/Skin-cancer-Analyzer-master/html_images/kjsce_header.jpeg")
-        img2 = Image.open("/Users/deepakhadkar/Documents/GitHub/Projects/Paid/Skin-cancer-Analyzer-master/html_images/names.jpeg")
+        img1 = Image.open("/Users/deepakhadkar/Documents/GitHub/Skin-Disease-Analyzer-master/html_images/kjsce_header.jpeg")
+        img2 = Image.open("/Users/deepakhadkar/Documents/GitHub/Skin-Disease-Analyzer-master/html_images/names.jpeg")
         
         st.image(img1, width=704)
         st.markdown(html_temp, unsafe_allow_html = True) 
@@ -138,7 +138,7 @@ def main():
         <style>
         body {
             background-color: #F5F5F5;
-            background-image: url('/Users/deepakhadkar/Documents/GitHub/Projects/Paid/Skin-cancer-Analyzer-master/html_images/background.jpg');
+            background-image: url('/Users/deepakhadkar/Documents/GitHub/Skin-Disease-Analyzer-master/html_images/background.jpg');
             background-size: cover;
             }
         </style>
